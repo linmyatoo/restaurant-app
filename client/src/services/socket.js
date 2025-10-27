802a43d865bbe3572397525588f9aa6ebe13ef55
naeyar <naeyar347@gmail.com>
2025-10-28 01:17:34 +0700

    Remove debug logs, clean up CORS configuration

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -3,10 +3,6 @@ import { io } from "socket.io-client";
 // Connect to your server - uses environment variable
 const URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
-// Debug: Log the URL being used
-console.log("🔌 Socket.IO connecting to:", URL);
-console.log("📝 Environment variable:", process.env.REACT_APP_API_URL);
-
 export const socket = io(URL, {
   autoConnect: false, // Don't connect automatically
 });
